24d198ce4a6750cc86bd0422e02a7a53e638c1c4
Tanmoy Roy <50512595+roytanmoy1@users.noreply.github.com>
2024-12-08 04:56:37 +0000

    Added logic for invoice creation and updation

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
-    ppr: 'incremental',
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'supreme-lamp-jqpx55jq6jr3pwxr-3000.app.github.dev', // Your Codespace URL
+        // '.app.github.dev', // Allows all Codespace URLs
+      ],
+    },
+    ppr: 'incremental'
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;

cd21ad9e30598c0a43b60c398ae66c5f5da03d4a
Tanmoy Roy <50512595+roytanmoy1@users.noreply.github.com>
2024-12-12 01:30:21 +0000

    Worked on accessibility and form validation and introduced eslint

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,76 +1,93 @@
-'use server';
-import {z} from 'zod';
-import {sql} from '@vercel/postgres';
-import {revalidatePath} from 'next/cache';
-import {redirect} from 'next/navigation';
+"use server";
+import { custom, z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending' , 'paid']),
-    date: z.string()
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0" }), // because we r coercing from str to number, empty string will become 0, gt func = greater than
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status",
+  }),
+  date: z.string(),
 });
 
-
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({id : true,date: true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Now we will validate the user entered data before sending it to db
 
-export async function createInvoice(formData: FormData) {
-    try{
-        // Now we will validate the user entered data before sending it to db
+  // Use Object.fromEntries(formData.entries()) for forms with a lot of entries
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // using safeparse to perform server side input validation
+    if (!validatedFields.success) {
+    //successfully performing input validation
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
 
-        // Use Object.fromEntries(formData.entries()) for forms with a lot of entries
-        const { customerId, amount, status } = CreateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
-        const dateVal = new Date().toISOString().split('T')[0];
+  const dateVal = new Date().toISOString().split("T")[0];
 
+  const amountInCents = amount * 100; // storing the amount in cents to avoid any floating point errors
 
-        const amountInCents = amount * 100; // storing the amount in cents to avoid any floating point errors
+  // now we have proper use entries prepared for the db insertion
+  try {
+    await sql`INSERT INTO invoices (customer_id,amount,status,date) VALUES (${customerId},${amountInCents},${status},${dateVal})`;
 
-
-        // now we have proper use entries prepared for the db insertion
-        await sql`INSERT INTO invoices (customer_id,amount,status,date) VALUES (${customerId},${amountInCents},${status},${dateVal})`;
-
-        // now we will clear the Client-side Router cache and trigger a new request to the server
-    }
-    catch(err){
-        console.log(err);
-    }
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    // now we will clear the Client-side Router cache and trigger a new request to the server
+  } catch (err) {
+    console.log(err);
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
-export async function updateInvoice(id:string,formData:FormData) {
-    try{
-        const { customerId, amount, status } = UpdateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
+export async function updateInvoice(id: string, formData: FormData) {
+  try {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get("customerId"),
+      amount: formData.get("amount"),
+      status: formData.get("status"),
+    });
 
-        const amountInCents = amount * 100; // storing the amount in cents to avoid any floating point errors
+    const amountInCents = amount * 100; // storing the amount in cents to avoid any floating point errors
 
-        await sql`UPDATE invoices set customer_id = ${customerId}, amount = ${amountInCents} , status = ${status} where id = ${id}`;
-    }
-    catch(err){
-        console.log(err);
-    }
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    await sql`UPDATE invoices set customer_id = ${customerId}, amount = ${amountInCents} , status = ${status} where id = ${id}`;
+  } catch (err) {
+    console.log(err);
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id:string) {
-    try{
-        await sql`DELETE from invoices where id = ${id}`;
-    }
-    catch(err){
-        console.log(err);
-    }
-    revalidatePath('/dashboard/invoices');
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE from invoices where id = ${id}`;
+  } catch (err) {
+    console.log(err);
+  }
+  revalidatePath("/dashboard/invoices");
 }
